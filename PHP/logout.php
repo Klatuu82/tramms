6e16e69318ac865705c63f187cf3ebbf761f5287
derray666 <33310969+derray666@users.noreply.github.com>
2017-11-08 11:36:43 +0100

    Update logout.php

--- PHP/logout.php
+++ PHP/logout.php
@@ -12,7 +12,12 @@
 			<form>
 			   <span style="margin-left:24px">  Sie sind erfolgreich ausgelogt<br ></span>
 			   <a class="waves-effect waves-light btn light-blue lighten-1 large" name="login" style="margin:auto; border-radius:25px;" href="../index.php">Zur&uuml;ck zur Startseite</a>
-			   <?php //header("refresh:7;../index.php") ?>
+			   <?php 
+				     <main>
+        			<audio id="audio_with_controls" controls src="zonk.mp4"
+        			type="audio/mp4" autoplay="true" >
+          			</main>
+				header("refresh:7;../index.php") ?>
 			</form>
 		   </div>
 	
